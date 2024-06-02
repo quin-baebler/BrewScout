73b45f61f7df404842123d096d68c5d9bff7c33b
OliviaSapp2 <114948288+OliviaSapp2@users.noreply.github.com>
2024-06-02 12:54:25 -0700

    Update FavoritesViewController.swift

--- BrewScout/BrewScout/FavoritesViewController.swift
+++ BrewScout/BrewScout/FavoritesViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     class TableCell : UITableViewCell{
         //cell components
         @IBOutlet weak var shopImage: UIImageView!
@@ -14,19 +14,34 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         @IBOutlet weak var shopLocationLabel: UILabel!
         @IBOutlet weak var otherInfoLabel: UILabel!
         @IBOutlet weak var filledHeartButton: UIButton!
+        var liked = true
+        @IBAction func clickHeartButton(_ sender: Any) {
+            // toggle the likes state
+            self.liked = !self.filledHeartButton.isSelected
+            // set the likes button accordingly
+            self.filledHeartButton.isSelected = self.liked
+            
+            self.filledHeartButton.setImage(UIImage(named: "heart.fill"), for: .normal)
+            self.filledHeartButton.setImage(UIImage(named: "heart"), for: .selected)
+        }
     }
 
+class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         tableView.dataSource = self
         tableView.delegate = self
         super.viewDidLoad()
-
+        liked = true
+        filteredFaves = cafeListFaves
         // Do any additional setup after loading the view.
     }
 
 
     @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var searchBar: UISearchBar!
+
     
     //temp hard coded list
     //need to change this to be adative to user input
@@ -35,7 +50,10 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         ("Cafe Alegro", "Seattle, WA", "Wifi, Bathroom, Hours 8am - 4pm"),
         ("Sip House", "Seattle, WA", "Wifi, Bathroom, Hours 8am - 5pm")
     ]
-    
+   
+    var filteredFaves = [(String, String, String)]()
+   // filteredFaves = cafeListFaves
+   
     var liked = true
     
     //click to like or unlike the caffe
@@ -63,18 +81,34 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cafeListFaves.count
+        return filteredFaves.count
     }
     
     //formating the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "exampleFavCell", for: indexPath) as! TableCell
-        let cafe = cafeListFaves[indexPath.row]
+        let cafe = filteredFaves[indexPath.row]
         cell.shopNameLabel.text = cafe.0
         cell.shopLocationLabel.text = cafe.1
         cell.otherInfoLabel.text = cafe.2
         cell.filledHeartButton.setImage(UIImage(named: "heart.fill"), for: .normal)
+        //cell.filledHeartButton.setImage(UIImage(named: "heart"), for: .selected)
+
         //add image and othercompenents
+
+        //add image and othercompenents
+        let imgName: String
+        switch cafe.0{
+        case "Cafe Solstice":
+            imgName = "solstice"
+        case "Cafe Allegro":
+            imgName = "allegro"
+        case "Sip House":
+            imgName = "sipHouse"
+        default:
+            imgName = ""
+        }
+        cell.shopImage.image = UIImage(named: imgName)
         
         return cell
     }
@@ -87,8 +121,21 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
     
     //set size
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 150
+            return 190
         }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        filteredFaves = []
+        if searchText == ""{
+            filteredFaves = cafeListFaves
+        }
+        for cafe in cafeListFaves{
+            if cafe.0.uppercased().contains(searchText.uppercased()){
+                filteredFaves.append(cafe)
+            }
+        }
+        self.tableView.reloadData()
+    }
     
     
     /*
