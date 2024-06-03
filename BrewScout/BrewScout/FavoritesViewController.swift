5f8722224b07e781bb4ea50539edb4a25ec554df
OliviaSapp2 <osapp2@uw.edu>
2024-06-03 13:58:39 -0700

    search bar for faves works

--- BrewScout/BrewScout/FavoritesViewController.swift
+++ BrewScout/BrewScout/FavoritesViewController.swift
@@ -26,8 +26,7 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         tableView.delegate = self
         searchBar.delegate = self
         super.viewDidLoad()
-        liked = true
-        filteredFaves = cafeListFaves
+//        liked = true
         // Do any additional setup after loading the view.
     }
 
@@ -48,33 +47,41 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
    
     var filteredFaves = [(String, String, String)]()
     var searchActive : Bool = false
-    var filtered: String = ""
+    //var filtered: String = ""
    
     var liked = true
     
+    func reloadData(){
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return filteredFaves.count
-        if(searchActive) {
-            return filteredFaves.count
+        return searchActive ? filteredFaves.count: cafeListFaves.count
+    }
+    func searchCafes(searchText: String){
+        //clear previous filter results
+        filteredFaves.removeAll()
+        
+        //filter the shop name based on the search text
+        let filteredNames = shopNamesFaves.filter {$0.localizedCaseInsensitiveContains(searchText as String)}
+        
+        //add matching cafes' data to filteredFaves
+        for cafe in cafeListFaves{
+            if filteredNames.contains(cafe.0){
+                filteredFaves.append(cafe)
+            }
         }
-        return cafeListFaves.count;
+        
+        //set search actiity to true if there is search text
+        searchActive = searchText.count > 0
     }
     
     //formating the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "exampleFavCell", for: indexPath) as! TableCell
-        //let cafe = filteredFaves[indexPath.row]
-        var cafe = cafeListFaves[indexPath.row]
-        if(searchActive){
-            for shop in cafeListFaves{
-                var temp = filtered
-                if shop.0.contains(String(temp)){
-                    filteredFaves.append(shop)
-                }
-            }
-             cafe = filteredFaves[indexPath.row]
-        }
         
+        let cafe = searchActive ? filteredFaves[indexPath.row] : cafeListFaves[indexPath.row]
+
         cell.shopNameLabel.text = cafe.0
         cell.shopLocationLabel.text = cafe.1
         cell.otherInfoLabel.text = cafe.2
@@ -84,15 +91,15 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
             cell.filledHeartButton.setImage(UIImage(named: "heart"), for: .normal)
         }
 
-        //add image and othercompenents
+        //add image
         let imgName: String
         switch cafe.0{
         case "Cafe Solstice":
-            imgName = "solstice"
+            imgName = "Solstice"
         case "Cafe Allegro":
             imgName = "allegro"
         case "Sip House":
-            imgName = "sipHouse"
+            imgName = "sip house"
         default:
             imgName = ""
         }
@@ -101,13 +108,14 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         return cell
     }
     
-   
+   //the like button does not work
     @IBAction func clickHeartButton(_ sender: UIButton) {
         if liked == true{
             liked = false
         } else {
             liked = true
         }
+        reloadData()
     }
     
     //change view to the shop page
@@ -121,36 +129,19 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
             return 190
         }
     
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-            searchActive = true;
-    }
-
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        searchActive = false;
-    }
-
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
         searchActive = false;
-    }
-
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchActive = false;
+        filteredFaves.removeAll()
+        tableView.reloadData()
     }
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-
-        filtered = (shopNamesFaves).filter({ (text) -> Bool in
-            let tmp: NSString = text as NSString
-            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
-            return range.location != NSNotFound
-        })
-        if(filtered.count == 0){
-            searchActive = false;
-        } else {
-            searchActive = true;
-        }
-        self.tableView.reloadData()
+        searchCafes(searchText: searchText as String)
+        tableView.reloadData()
     }
+    
+    
     
     /*
     // MARK: - Navigation
